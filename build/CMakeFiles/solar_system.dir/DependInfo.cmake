403a695bda8355d9d03d6273a33b6878d1500d89
Youssef Nidabrahim <y.nidabrahim@piman2-0.fr>
2019-01-19 05:27:06 +0100

    Add cmake

---
+++ build/CMakeFiles/solar_system.dir/DependInfo.cmake
@@ -0,0 +1,36 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/youssef/ISIMA/Infographie/tp1/opengl-labs/src/engine/Engine.cpp" "/home/youssef/ISIMA/Infographie/tp1/opengl-labs/build/CMakeFiles/solar_system.dir/src/engine/Engine.cpp.o"
+  "/home/youssef/ISIMA/Infographie/tp1/opengl-labs/src/engine/EventHandler.cpp" "/home/youssef/ISIMA/Infographie/tp1/opengl-labs/build/CMakeFiles/solar_system.dir/src/engine/EventHandler.cpp.o"
+  "/home/youssef/ISIMA/Infographie/tp1/opengl-labs/src/engine/camera.cpp" "/home/youssef/ISIMA/Infographie/tp1/opengl-labs/build/CMakeFiles/solar_system.dir/src/engine/camera.cpp.o"
+  "/home/youssef/ISIMA/Infographie/tp1/opengl-labs/src/engine/glmUtils.cpp" "/home/youssef/ISIMA/Infographie/tp1/opengl-labs/build/CMakeFiles/solar_system.dir/src/engine/glmUtils.cpp.o"
+  "/home/youssef/ISIMA/Infographie/tp1/opengl-labs/src/engine/shader.cpp" "/home/youssef/ISIMA/Infographie/tp1/opengl-labs/build/CMakeFiles/solar_system.dir/src/engine/shader.cpp.o"
+  "/home/youssef/ISIMA/Infographie/tp1/opengl-labs/src/engine/shader_object.cpp" "/home/youssef/ISIMA/Infographie/tp1/opengl-labs/build/CMakeFiles/solar_system.dir/src/engine/shader_object.cpp.o"
+  "/home/youssef/ISIMA/Infographie/tp1/opengl-labs/src/engine/texture_object.cpp" "/home/youssef/ISIMA/Infographie/tp1/opengl-labs/build/CMakeFiles/solar_system.dir/src/engine/texture_object.cpp.o"
+  "/home/youssef/ISIMA/Infographie/tp1/opengl-labs/src/geometry/cube.cpp" "/home/youssef/ISIMA/Infographie/tp1/opengl-labs/build/CMakeFiles/solar_system.dir/src/geometry/cube.cpp.o"
+  "/home/youssef/ISIMA/Infographie/tp1/opengl-labs/src/geometry/sphere.cpp" "/home/youssef/ISIMA/Infographie/tp1/opengl-labs/build/CMakeFiles/solar_system.dir/src/geometry/sphere.cpp.o"
+  "/home/youssef/ISIMA/Infographie/tp1/opengl-labs/src/main.cpp" "/home/youssef/ISIMA/Infographie/tp1/opengl-labs/build/CMakeFiles/solar_system.dir/src/main.cpp.o"
+  "/home/youssef/ISIMA/Infographie/tp1/opengl-labs/src/models/earth.cpp" "/home/youssef/ISIMA/Infographie/tp1/opengl-labs/build/CMakeFiles/solar_system.dir/src/models/earth.cpp.o"
+  "/home/youssef/ISIMA/Infographie/tp1/opengl-labs/src/models/moon.cpp" "/home/youssef/ISIMA/Infographie/tp1/opengl-labs/build/CMakeFiles/solar_system.dir/src/models/moon.cpp.o"
+  "/home/youssef/ISIMA/Infographie/tp1/opengl-labs/src/models/skybox.cpp" "/home/youssef/ISIMA/Infographie/tp1/opengl-labs/build/CMakeFiles/solar_system.dir/src/models/skybox.cpp.o"
+  "/home/youssef/ISIMA/Infographie/tp1/opengl-labs/src/models/sun.cpp" "/home/youssef/ISIMA/Infographie/tp1/opengl-labs/build/CMakeFiles/solar_system.dir/src/models/sun.cpp.o"
+  "/home/youssef/ISIMA/Infographie/tp1/opengl-labs/src/solar_system.cpp" "/home/youssef/ISIMA/Infographie/tp1/opengl-labs/build/CMakeFiles/solar_system.dir/src/solar_system.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../src"
+  "/usr/include/SDL2"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/solar_system.dir/cmake_clean.cmake
@@ -0,0 +1,24 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/solar_system.dir/src/main.cpp.o"
+  "CMakeFiles/solar_system.dir/src/solar_system.cpp.o"
+  "CMakeFiles/solar_system.dir/src/engine/camera.cpp.o"
+  "CMakeFiles/solar_system.dir/src/engine/Engine.cpp.o"
+  "CMakeFiles/solar_system.dir/src/engine/EventHandler.cpp.o"
+  "CMakeFiles/solar_system.dir/src/engine/glmUtils.cpp.o"
+  "CMakeFiles/solar_system.dir/src/engine/shader_object.cpp.o"
+  "CMakeFiles/solar_system.dir/src/engine/texture_object.cpp.o"
+  "CMakeFiles/solar_system.dir/src/engine/shader.cpp.o"
+  "CMakeFiles/solar_system.dir/src/geometry/sphere.cpp.o"
+  "CMakeFiles/solar_system.dir/src/geometry/cube.cpp.o"
+  "CMakeFiles/solar_system.dir/src/models/earth.cpp.o"
+  "CMakeFiles/solar_system.dir/src/models/moon.cpp.o"
+  "CMakeFiles/solar_system.dir/src/models/skybox.cpp.o"
+  "CMakeFiles/solar_system.dir/src/models/sun.cpp.o"
+  "solar_system.pdb"
+  "solar_system"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/solar_system.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
