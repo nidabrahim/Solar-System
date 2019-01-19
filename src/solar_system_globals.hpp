b06b641321df4ceda3c003850f26d60aad776c9a
Youssef Nidabrahim <y.nidabrahim@piman2-0.fr>
2019-01-19 05:24:07 +0100

    Update global constants path

--- src/solar_system_globals.hpp
+++ src/solar_system_globals.hpp
@@ -10,20 +10,20 @@ using std::string;
  */
 
 //! Earth Constants for shaders files
-const string EARTH_SHADER_VERT_FILENAME = "includes/shaders/earth.vert";
-const string EARTH_SHADER_FRAG_FILENAME = "includes/shaders/earth.frag";
+const string EARTH_SHADER_VERT_FILENAME = "../includes/shaders/earth.vert";
+const string EARTH_SHADER_FRAG_FILENAME = "../includes/shaders/earth.frag";
 
 //! Moon Constants for shaders files
-const string MOON_SHADER_VERT_FILENAME = "includes/shaders/moon.vert";
-const string MOON_SHADER_FRAG_FILENAME = "includes/shaders/moon.frag";
+const string MOON_SHADER_VERT_FILENAME = "../includes/shaders/moon.vert";
+const string MOON_SHADER_FRAG_FILENAME = "../includes/shaders/moon.frag";
 
 //! Skybox Constants for shaders files
-const string SKYBOX_SHADER_VERT_FILENAME = "includes/shaders/skybox.vert";
-const string SKYBOX_SHADER_FRAG_FILENAME = "includes/shaders/skybox.frag";
+const string SKYBOX_SHADER_VERT_FILENAME = "../includes/shaders/skybox.vert";
+const string SKYBOX_SHADER_FRAG_FILENAME = "../includes/shaders/skybox.frag";
 
 //! Sun Constants for shaders files
-const string SUN_SHADER_VERT_FILENAME = "includes/shaders/sun.vert";
-const string SUN_SHADER_FRAG_FILENAME = "includes/shaders/sun.frag";
+const string SUN_SHADER_VERT_FILENAME = "../includes/shaders/sun.vert";
+const string SUN_SHADER_FRAG_FILENAME = "../includes/shaders/sun.frag";
 
 /**
  * @brief Textures Global Constants
@@ -31,38 +31,38 @@ const string SUN_SHADER_FRAG_FILENAME = "includes/shaders/sun.frag";
  */
 
 //! Earth Constants for textures files
-const string EARTH_TEXTURES_MORNING_XP_FILENAME = "includes/textures/earth/morning/earth_xp.png";
-const string EARTH_TEXTURES_MORNING_XN_FILENAME = "includes/textures/earth/morning/earth_xn.png";
-const string EARTH_TEXTURES_MORNING_YP_FILENAME = "includes/textures/earth/morning/earth_yp.png";
-const string EARTH_TEXTURES_MORNING_YN_FILENAME = "includes/textures/earth/morning/earth_yn.png";
-const string EARTH_TEXTURES_MORNING_ZP_FILENAME = "includes/textures/earth/morning/earth_zp.png";
-const string EARTH_TEXTURES_MORNING_ZN_FILENAME = "includes/textures/earth/morning/earth_zn.png";
+const string EARTH_TEXTURES_MORNING_XP_FILENAME = "../includes/textures/earth/morning/earth_xp.png";
+const string EARTH_TEXTURES_MORNING_XN_FILENAME = "../includes/textures/earth/morning/earth_xn.png";
+const string EARTH_TEXTURES_MORNING_YP_FILENAME = "../includes/textures/earth/morning/earth_yp.png";
+const string EARTH_TEXTURES_MORNING_YN_FILENAME = "../includes/textures/earth/morning/earth_yn.png";
+const string EARTH_TEXTURES_MORNING_ZP_FILENAME = "../includes/textures/earth/morning/earth_zp.png";
+const string EARTH_TEXTURES_MORNING_ZN_FILENAME = "../includes/textures/earth/morning/earth_zn.png";
 
-const string EARTH_TEXTURES_NIGHT_XP_FILENAME = "includes/textures/earth/night/earth_xp.png";
-const string EARTH_TEXTURES_NIGHT_XN_FILENAME = "includes/textures/earth/night/earth_xn.png";
-const string EARTH_TEXTURES_NIGHT_YP_FILENAME = "includes/textures/earth/night/earth_yp.png";
-const string EARTH_TEXTURES_NIGHT_YN_FILENAME = "includes/textures/earth/night/earth_yn.png";
-const string EARTH_TEXTURES_NIGHT_ZP_FILENAME = "includes/textures/earth/night/earth_zp.png";
-const string EARTH_TEXTURES_NIGHT_ZN_FILENAME = "includes/textures/earth/night/earth_zn.png";
+const string EARTH_TEXTURES_NIGHT_XP_FILENAME = "../includes/textures/earth/night/earth_xp.png";
+const string EARTH_TEXTURES_NIGHT_XN_FILENAME = "../includes/textures/earth/night/earth_xn.png";
+const string EARTH_TEXTURES_NIGHT_YP_FILENAME = "../includes/textures/earth/night/earth_yp.png";
+const string EARTH_TEXTURES_NIGHT_YN_FILENAME = "../includes/textures/earth/night/earth_yn.png";
+const string EARTH_TEXTURES_NIGHT_ZP_FILENAME = "../includes/textures/earth/night/earth_zp.png";
+const string EARTH_TEXTURES_NIGHT_ZN_FILENAME = "../includes/textures/earth/night/earth_zn.png";
 
 //! Moon Constants for textures files
-const string MOON_TEXTURES_XP_FILENAME = "includes/textures/moon/moon_xp.png";
-const string MOON_TEXTURES_XN_FILENAME = "includes/textures/moon/moon_xn.png";
-const string MOON_TEXTURES_YP_FILENAME = "includes/textures/moon/moon_yp.png";
-const string MOON_TEXTURES_YN_FILENAME = "includes/textures/moon/moon_yn.png";
-const string MOON_TEXTURES_ZP_FILENAME = "includes/textures/moon/moon_zp.png";
-const string MOON_TEXTURES_ZN_FILENAME = "includes/textures/moon/moon_zn.png";
+const string MOON_TEXTURES_XP_FILENAME = "../includes/textures/moon/moon_xp.png";
+const string MOON_TEXTURES_XN_FILENAME = "../includes/textures/moon/moon_xn.png";
+const string MOON_TEXTURES_YP_FILENAME = "../includes/textures/moon/moon_yp.png";
+const string MOON_TEXTURES_YN_FILENAME = "../includes/textures/moon/moon_yn.png";
+const string MOON_TEXTURES_ZP_FILENAME = "../includes/textures/moon/moon_zp.png";
+const string MOON_TEXTURES_ZN_FILENAME = "../includes/textures/moon/moon_zn.png";
 
 //! Skybox Constants for textures files
-const string SKYBOX_TEXTURES_XP_FILENAME = "includes/textures/skybox/skybox_xp.png";
-const string SKYBOX_TEXTURES_XN_FILENAME = "includes/textures/skybox/skybox_xn.png";
-const string SKYBOX_TEXTURES_YP_FILENAME = "includes/textures/skybox/skybox_yp.png";
-const string SKYBOX_TEXTURES_YN_FILENAME = "includes/textures/skybox/skybox_yn.png";
-const string SKYBOX_TEXTURES_ZP_FILENAME = "includes/textures/skybox/skybox_zp.png";
-const string SKYBOX_TEXTURES_ZN_FILENAME = "includes/textures/skybox/skybox_zn.png";
+const string SKYBOX_TEXTURES_XP_FILENAME = "../includes/textures/skybox/skybox_xp.png";
+const string SKYBOX_TEXTURES_XN_FILENAME = "../includes/textures/skybox/skybox_xn.png";
+const string SKYBOX_TEXTURES_YP_FILENAME = "../includes/textures/skybox/skybox_yp.png";
+const string SKYBOX_TEXTURES_YN_FILENAME = "../includes/textures/skybox/skybox_yn.png";
+const string SKYBOX_TEXTURES_ZP_FILENAME = "../includes/textures/skybox/skybox_zp.png";
+const string SKYBOX_TEXTURES_ZN_FILENAME = "../includes/textures/skybox/skybox_zn.png";
 
 //! SUN Constants for textures files
-const string SUN_TEXTURES_FILENAME = "includes/textures/sun/sun_colors.png";
+const string SUN_TEXTURES_FILENAME = "../includes/textures/sun/sun_colors.png";
 
 
 #define SOLAR_SYSTEM_H
